90671a90c8d965192e7e4059155ee75722906cfe
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2026-07-05 17:56:28 -0400

    Add MQTT Manager for creating and connecting to EC2, reorganize mqtt folder

---
+++ app/mqtt/client.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+from app.core.mqtt_config import (
+    MQTT_USERNAME,
+    MQTT_PASSWORD,
+    MQTT_BROKER_HOST,
+    MQTT_BROKER_PORT,
+    CAFILE,
+    CERTFILE,
+    KEYFILE,
+)
+
+class MQTTManager:
+    def __init__(self, settings, id: str):
+        self.settings = settings
+        self.id = id
+        self.client = self.create_client()
+        self.connected = False
+
+    def create_client(self):
+        client = mqtt.Client( mqtt.CallbackAPIVersion.VERSION2, client_id=self.id)
+
+        client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+        client.tls_set(ca_certs=CAFILE, certfile=CERTFILE, keyfile=KEYFILE)
+
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+
+        return client
+
+    def on_connect(self, client, userdata, flags, reason_code, properties):
+        print(f"Connected with result code {reason_code}")
+        self.connected = True
+
+    def on_message(self, client, userdata, msg):
+        print("Message received on topic " + msg.topic + ": " + msg.payload.decode())
+
+    def connect(self):
+        self.client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
+        self.client.loop_start()
+
+    def publish(self, topic: str, msg: str):
+        if not self.connected:
+            print("Error: client is not connected.\n")
+            return
+
+        self.client.publish(topic, msg)
+
+    def disconnect(self):
+        self.client.loop_stop()
+        self.client.disconnect()
+        self.connected = False
